388d9f1f6db3348a6b338880764d7e08f0251d4f
MrDevil <84269662+Mr-Devil7@users.noreply.github.com>
2025-06-13 18:11:55 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'pop-up': 'popUp 0.3s ease-out forwards',
+      },
+      keyframes: {
+        popUp: {
+          '0%': { transform: 'scale(0.95)', opacity: '0' },
+          '100%': { transform: 'scale(1)', opacity: '1' },
+        },
+      },
+    },
   },
   plugins: [],
 };
